1616f8722b0a9b4b36f61aa1eab64f41da950cbf
squisquad <yannpatrick@MacBook-Pro-di-Yann.local>
2017-11-06 15:51:43 +0100

    final updates website

--- web/sketch.js
+++ web/sketch.js
@@ -1,5 +1,17 @@
 function setup() {
 	noCanvas();
+	let lang = navigator.language || 'en-US'
+	let speechRec = new p5.SpeechRec(lang, gotSpeech);
+	let continuous = true;
+	let interim = false;
+	speechRec.start(continuous, interim);
+	function gotSpeech(){
+		if(speechRec.resultValue){
+			let txt = speechRec.resultString;
+			console.log(txt);
+			document.getElementById("myspeech").innerHTML = txt;
+		}
+	}
 }
 
 function draw() {
